a5b2d1d5322fb464a79e6827e752dad847ecb40c
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2025-03-13 13:36:57 +0000

    First commit, added necessary files

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
 app.use(express.json())
-app.usee(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
 
